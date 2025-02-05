73b8fc604fe2683901b6b5d906d1e80eed95e228
Mohnish Sharma <mohnish.unity3d.dev@gmail.com>
2025-02-05 05:59:46 +0530

    setup for the xcode

--- build_scripts/macos/build_cmake.zsh
+++ build_scripts/macos/build_cmake.zsh
@@ -6,10 +6,19 @@ source "$(dirname "$0")/_variables.zsh"
 # Parse arguments
 arg="$1"
 
+# Code Gen Specific
+code_gen=""
+is_xcode=0
+enable_tsan=0
 # Determine build type
 is_release=0
 if [[ "$arg" == "rel" || "$arg" == "release" || "$arg" == "r" ]]; then
     is_release=1
+elif [[ "$arg" == "xcode" ]]; then
+    code_gen="-GXcode"
+    is_xcode=1
+elif [[ "$arg" == "tsan" ]]; then
+    enable_tsan=1
 fi
 
 # Adjust project root path
@@ -27,22 +36,37 @@ fi
 echo "PROJECT_ROOT_DIR_PATH=$PROJECT_ROOT_DIR_PATH; BIN_DIR_PATH=$BIN_DIR_PATH"
 # cd "$BIN_DIR_PATH" || exit 1
 if [[ $is_release -eq 0 ]]; then
-    cmake -DUSE_DEBUG=ON \
-          -DCMAKE_BUILD_TYPE=Debug \
-          -S "$PROJECT_ROOT_DIR_PATH" \
-          -B "$BIN_DIR_PATH"
+    if [[ enable_tsan -eq 1 ]]; then
+        echo "TSan is ON"
+        cmake -DCMAKE_C_COMPILER=clang -DCMAKE_CXX_COMPILER=clang++ \
+              -DUSE_DEBUG=ON -DENABLE_TSAN=ON \
+              -DCMAKE_BUILD_TYPE=Debug \
+              -S "$PROJECT_ROOT_DIR_PATH" \
+              -B "$BIN_DIR_PATH"
+    else
+        cmake -DUSE_DEBUG=ON \
+              -DCMAKE_BUILD_TYPE=Debug \
+              -S "$PROJECT_ROOT_DIR_PATH" \
+              -B "$BIN_DIR_PATH" $code_gen
+    fi
 else
     cmake -DUSE_DEBUG=OFF \
           -DCMAKE_BUILD_TYPE=Release \
           -S "$PROJECT_ROOT_DIR_PATH" \
-          -B "$BIN_DIR_PATH"
+          -B "$BIN_DIR_PATH" $code_gen
 fi
 
 cd "$BIN_DIR_PATH" || exit 1
-# Build CMake files with Make
-make
 
-mv $BIN_DIR_PATH/compile_commands.json $PROJECT_ROOT_DIR_PATH/compile_commands.json
+# Build CMake files with Make
+if [[ is_xcode -eq 0 ]]; then
+    make
+    # for vscode clangd linter. only works with make(?)
+    mv $BIN_DIR_PATH/compile_commands.json $PROJECT_ROOT_DIR_PATH/compile_commands.json
+else
+    xcodebuild -project $BIN_DIR_PATH/$PROJECT_NAME.xcodeproj
+    open $BIN_DIR_PATH/$PROJECT_NAME.xcodeproj
+fi
 
 # Return to the previous directory if not inside the bin directory
 if [[ $InsideBin -eq 0 ]]; then
